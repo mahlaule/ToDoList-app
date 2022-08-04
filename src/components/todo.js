83ecb1507468b546f244c2229094fbaf6d19dbb7
unknown <kevinmahlaule@gmail.com>
2022-08-04 12:04:55 +0200

    update

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,32 +1,43 @@
-import {FormHelperText,DeleteIcon,deleteDoc, InputLabel, TextField,Select,MenuItem, FormControl,Button,List , ListItem, ListItemAvatar, ListItemText} from "@mui/material";
+import {FormHelperText,DeleteIcon, InputLabel, TextField,Select,MenuItem, FormControl,Button,List , ListItem, ListItemAvatar, ListItemText} from "@mui/material";
 import { useEffect, useState } from "react";
 import {db} from "../config/firebase"
 import firebase from 'firebase/compat/app';
 import {getFirestore} from "firebase/firestore"
-import { collection,doc,query,where, updateDoc, addDoc,getDoc,getDocs, onSnapshot, serverTimestamp } from "firebase/firestore"; 
+import { collection,doc,query,where, deleteDoc,updateDoc, addDoc,getDoc,getDocs, onSnapshot, serverTimestamp } from "firebase/firestore"; 
 import Home from "./home";
 import React from "react";
+import { async } from "@firebase/util";
 
 export default function TodoListItem({todo,inprogress,id}){
     
 
     function toggleInProgress(){
         
-        const docRef =  updateDoc(collection(db, "todos"), {
-            inprogress: !inprogress
-    })
+      db.collection("todos").doc(id).update({
+        inprogress:!inprogress
+
+      })
+
+}
+function deleteTodo(){
+   
+
+    deleteDoc(doc(db, "todos", "inprogress"));
+
+
 }
     
 
     return (
         <div style={{display:"flex"}}>
-        <ListItem>
-            <ListItemText primary ={todo} secondary={inprogress ? "in progress": "completed"}/>
+        <ListItem style={{width:"65%"}}>
+            <ListItemText style={{marginLeft:"600px", border:"2px solid gray"}} primary ={todo} secondary={inprogress ?  "in progress": "completed"}/>
 
             
        
         </ListItem>
         <Button onClick={toggleInProgress}>{inprogress ? "DONE": "UNDONE"}</Button>
+        <Button onClick={ deleteTodo}>completed</Button>
         </div>
     )
     }
